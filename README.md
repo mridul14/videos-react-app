ada30fafe5341fca169a658d8e1767487ca249f4
mridul14 <mridultripathi1408@gmail.com>
2021-08-19 20:21:34 +0530

    Initial Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+This Application was created as part of course <a href="https://www.udemy.com/course/react-redux/" target="_blank">Master React and Redux with React Router, Webpack, and Create-React-App. Includes Hooks!</a> by Stephen Grider on Udemy.
---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+    state = { term: '' };
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // TODO: make sure we call callback from parent component
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit} >
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type='text' value={this.state.term} onChange={(event) => this.setState({ term: event.target.value })}/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+export default SearchBar;
